27d914bda30212d3dc73979f37977bb4fcf50e62
zhaokel <icyaneon@gmail.com>
2019-11-15 09:37:25 -0800

    Deliverable 11 wip

--- src/main/model/Institution.java
+++ src/main/model/Institution.java
@@ -39,35 +39,6 @@ public class Institution extends Observable {
         addObserver(obs);
     }
 
-    // EFFECTS: prints out string from web
-    public void welcome() throws IOException {
-        // as instructed, code quoted from: http://zetcode.com/articles/javareadwebpage/
-        BufferedReader br = null;
-
-        try {
-            String theURL = "https://www.students.cs.ubc.ca/~cs-210/2018w1/welcomemsg.html"; //this can point to any URL
-            URL url = new URL(theURL);
-            br = new BufferedReader(new InputStreamReader(url.openStream()));
-
-            String line;
-
-            StringBuilder sb = new StringBuilder();
-
-            while ((line = br.readLine()) != null) {
-
-                sb.append(line);
-                sb.append(System.lineSeparator());
-            }
-
-            System.out.println(sb.substring(0, sb.length() - 14));
-        } finally {
-
-            if (br != null) {
-                br.close();
-            }
-        }
-    }
-
     // EFFECTS: trigger fire alarm in building
     public Boolean fire() {
         return building.fire();
